16b9f17292d0c411f629c296c7c8c82beaef640f
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2025-05-15 13:37:22 +0100

    Update api_service.dart

--- api_service.dart
+++ api_service.dart
@@ -4,7 +4,13 @@ import '../models/earthquake.dart';
 
 class ApiService {
   static const String apiUrl =
-      "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10";
+      "https://earthquake.usgs.gov/fdsnws/event/1/query"
+      "?format=geojson"
+      "&starttime=2024-01-01"
+      "&endtime=2025-01-01"
+      "&minmagnitude=4"
+      "&orderby=time";
+
 
   static Future<List<Earthquake>> fetchEarthquakes() async {
     final response = await http.get(Uri.parse(apiUrl));
